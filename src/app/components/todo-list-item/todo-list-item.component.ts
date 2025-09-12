a14bbfdb889003b6d77bd49f66de14121d2c574e
pablo <tonyp2hoy@gmail.com>
2025-09-11 23:55:18 -0400

    fix en estilo date time

--- src/app/components/todo-list-item/todo-list-item.component.ts
+++ src/app/components/todo-list-item/todo-list-item.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Todo } from 'src/app/interfaces/todos.interfaces';
+import { DatePipe } from "../../pipes/date.pipe";
 
 @Component({
   selector: 'app-todo-list-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DatePipe],
   templateUrl: './todo-list-item.component.html',
   styleUrls: ['./todo-list-item.component.css'],
 })
