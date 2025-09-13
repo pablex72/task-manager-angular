0c981fd3b756de5967e7aec338d6f63d2df843d5
pablo <tonyp2hoy@gmail.com>
2025-09-13 02:55:23 -0400

    adicionando pipe date formatter

--- src/app/pipes/date.pipe.ts
+++ src/app/pipes/date.pipe.ts
@@ -8,6 +8,30 @@ export class DatePipe implements PipeTransform {
 
   transform(value: string | Date): string {
     let inputDate: Date;
+
+    if(typeof value === "string"){
+      inputDate = new Date(value)
+
+      if(isNaN(inputDate.getDate())) return '';
+    } else if(value instanceof Date) {
+      inputDate = value;
+    } else return '';
+
+    const currentDate = new Date()
+
+    if(this.isSameDay(currentDate, inputDate)){
+      return 'Hoy'
+    } else if (this.isYesterday(currentDate, inputDate)){
+      return 'Ayer'
+    } else {
+      const options: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+      }
+
+      return inputDate.toLocaleDateString(undefined, options)
+    }
   }
 
   private isSameDay(date1: Date, date2: Date): boolean {
@@ -23,8 +47,10 @@ export class DatePipe implements PipeTransform {
     yesterday.setDate(yesterday.getDate() - 1);
 
     return (
-      date2.getDate() === yesterday.getDate
-    )
+      date2.getDate() === yesterday.getDate() &&
+      date2.getMonth() === yesterday.getMonth() &&
+      date2.getFullYear() === yesterday.getFullYear()
+    );
   }
 
 }
